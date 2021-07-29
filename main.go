a5bc4d726383f2e2ea48ff25d9c4da29e92246f1
alexander <zeimedee@gmail.com>
2021-07-29 16:12:20 +0000

    :rocket: interview

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/zeimedee/koa_test/handlers"
 )
 
@@ -13,5 +15,10 @@ func Handler(app *fiber.App) {
 func main() {
 	app := fiber.New()
 	Handler(app)
+
+	app.Use(logger.New(logger.Config{
+		Format: "${url} ${status} - ${method}/n",
+		Output: os.Stdout,
+	}))
 	log.Fatal(app.Listen(":3000"))
 }
